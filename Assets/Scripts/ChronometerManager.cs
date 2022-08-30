7f482b99078b6597b6d5ea24fa783588005c4965
Taha Kızmaz <tahakizmaz@gmail.com>
2022-08-30 05:46:32 +0300

    feat(logic): Add timers for minute and seconds

---
+++ Assets/Scripts/ChronometerManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChronometerManager : MonoBehaviour
+{
+    public bool isChronometerActive = false;
+
+    [SerializeField]
+    private Transform secondHand;
+
+    [SerializeField]
+    private Transform minuteHand;
+
+    private float timer = 0f;
+    private int timeToDegreeMultiplier = 6;
+    private int secondsInAMinute = 60;
+    
+    void Update()
+    {
+        if (isChronometerActive)
+        {
+            timer += Time.deltaTime;
+            secondHand.eulerAngles = new Vector3(0f, 0f, -timer * timeToDegreeMultiplier);
+            minuteHand.eulerAngles = new Vector3(0f, 0f, -((int)timer / secondsInAMinute) * timeToDegreeMultiplier);
+        }
+    }
+
+    public void StartTime()
+    {
+        isChronometerActive = true;
+    }
+
+    public void StopTime()
+    {
+        isChronometerActive = false;
+        ResetHands();
+    }
+
+    private void ResetHands()
+    {
+        secondHand.eulerAngles = new Vector3(0f, 0f, 0f);
+        minuteHand.eulerAngles = new Vector3(0f, 0f, 0f);
+    }
+}
